d643e648f6b78b3b6b2ea30dc386136565de793b
Milan Lophy <milanlophy@gmail.com>
2025-07-23 17:15:19 +0530

    feat: add react app

--- usecallback/src/Components/parentComp.js
+++ usecallback/src/Components/parentComp.js
@@ -73,19 +73,12 @@ export function ParentComp(){
     const [age, setAge]= useState(25)
     const [salary, setSalary]= useState(50000)
 
-    const incrementAge= useCallback(()=>{
-        setAge(age + 1)
-    },[age])
-    const incrementSalary= useCallback(()=>{
-        setSalary(salary + 1000)
-    }, [salary])
+    const incrementAge
     return(
         <div>
             <Title/>
             <Count text='Age' count={age}/>
             <Button clickHandler={incrementAge}>Increment Age</Button>
-            <Count text='Salary' count={salary}/>
-            <Button clickHandler={incrementSalary}>Increment Salary</Button>
         </div>
     )
 }
